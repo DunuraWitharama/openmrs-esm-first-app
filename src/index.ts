319095c75b1dd653c8482829e257e8a64817bb32
Dunura Witharama <dunurawitharama@gmail.com>
2026-09-23 16:35:51 +0530

    My first OpenMRS module

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,10 @@
 import { getAsyncLifecycle, defineConfigSchema } from '@openmrs/esm-framework';
 import { configSchema } from './config-schema';
 
-const moduleName = '@openmrs/esm-template-app';
+const moduleName = '@dunura99/esm-first-app';
 
 const options = {
-  featureName: 'root-world',
+  featureName: 'first-app',
   moduleName,
 };
 
